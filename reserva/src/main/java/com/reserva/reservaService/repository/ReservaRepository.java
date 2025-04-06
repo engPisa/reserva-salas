86df92b1745e8dc7545373849ef6d45b46ad1384
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-04-06 18:37:35 -0300

    Feat: implementation reserva repository

--- reserva/src/main/java/com/reserva/reservaService/repository/ReservaRepository.java
+++ reserva/src/main/java/com/reserva/reservaService/repository/ReservaRepository.java
@@ -1,4 +1,12 @@
 package com.reserva.reservaService.repository;
 
-public class ReservaRepository {
+import com.reserva.reservaService.model.ReservaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<ReservaModel, Long> {
+    boolean salaOcupada(Long salaId, LocalDateTime dataHora);
 }
