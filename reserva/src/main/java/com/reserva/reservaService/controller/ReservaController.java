b7ed0510b4de8ea4731ae7e4e39b44bcf62cb3c6
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-04-06 18:37:57 -0300

    Feat: implementation reservaController.java

--- reserva/src/main/java/com/reserva/reservaService/controller/ReservaController.java
+++ reserva/src/main/java/com/reserva/reservaService/controller/ReservaController.java
@@ -1,4 +1,28 @@
 package com.reserva.reservaService.controller;
 
+import com.reserva.reservaService.DTO.ReservaRequestDTO;
+import com.reserva.reservaService.DTO.ReservaResponseDTO;
+import com.reserva.reservaService.services.ReservaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reserva-sala/reserva")
 public class ReservaController {
+
+    private final ReservaService reservaService;
+
+    public ReservaController(ReservaService reservaService) {
+        this.reservaService = reservaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservaResponseDTO>cadastrar(@RequestBody ReservaRequestDTO dto){
+        ReservaResponseDTO responseDTO = reservaService.cadastrarReserva(dto);
+        return ResponseEntity.ok(responseDTO);
+    }
+
 }
