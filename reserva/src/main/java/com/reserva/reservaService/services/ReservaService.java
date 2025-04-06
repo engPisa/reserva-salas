2d6a5b77e1ee554dbea01437a9d6f0e0ea6c7939
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-04-06 18:36:57 -0300

    Feat: implementation reserva service

--- reserva/src/main/java/com/reserva/reservaService/services/ReservaService.java
+++ reserva/src/main/java/com/reserva/reservaService/services/ReservaService.java
@@ -1,4 +1,41 @@
 package com.reserva.reservaService.services;
 
+import com.reserva.reservaService.DTO.ReservaRequestDTO;
+import com.reserva.reservaService.DTO.ReservaResponseDTO;
+import com.reserva.reservaService.model.ReservaModel;
+import com.reserva.reservaService.repository.ReservaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservaService {
+
+    private final ReservaRepository reservaRepository;
+
+    public ReservaService(ReservaRepository reservaRepository) {
+        this.reservaRepository = reservaRepository;
+    }
+
+    @Transactional
+    public ReservaResponseDTO cadastrarReserva(ReservaRequestDTO dto) {
+        if (reservaRepository.salaOcupada(dto.salaId(), dto.dataHora())){
+            throw new IllegalArgumentException("Ja existe uma reserva nesse horario.");
+        }
+
+        ReservaModel reserva = new ReservaModel();
+        reserva.setSalaId(dto.salaId());
+        reserva.setDataHora(dto.dataHora());
+        reserva.setUsuarioId(dto.usuarioId());
+
+        reserva = reservaRepository.save(reserva);
+
+        return new ReservaResponseDTO(
+                reserva.getId(),
+                reserva.getDataHora(),
+                reserva.getSalaId(),
+                reserva.getUsuarioId()
+        );
+    }
+
 }
+
