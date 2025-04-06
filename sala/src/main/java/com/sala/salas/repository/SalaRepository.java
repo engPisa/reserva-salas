d0b15c57ddf1342e81886e233e3f7ed632b1f696
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-04-06 20:46:27 -0300

    Feat: Implementation sala repository

--- sala/src/main/java/com/sala/salas/repository/SalaRepository.java
+++ sala/src/main/java/com/sala/salas/repository/SalaRepository.java
@@ -1,4 +1,8 @@
 package com.sala.salas.repository;
 
-public class SalaRepository {
+import com.sala.salas.model.SalaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaRepository extends JpaRepository<SalaModel, Long> {
+    boolean existsNome(String nome, String localizacao);
 }
